42be6941020f3bf0404b9f1f5653e67019011951
Vinicius Dyonisio <viniciusdyo@gmail.com>
2021-07-04 23:46:22 -0300

    Project Upload
    Subindo Projeto

---
+++ CrashCourse/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CrashCourse.Models;
+
+namespace CrashCourse.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CrashCourse.Models.Joke> Joke { get; set; }
+    }
+}
